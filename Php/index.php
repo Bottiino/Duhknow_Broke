ef366a2e21420b2877e5df9ad5ef35532baa5850
Bottino <adriancollins59@live.com>
2018-10-21 11:30:38 +0100

    Very basic HTML for index, login/register and language pages

--- Php/index.php
+++ Php/index.php
@@ -10,6 +10,14 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <p>Hello</p>
+        <div>
+            <p>Logo</p>
+        </div>
+        <div>
+            <a href="language.php" class="enter">Play</a>
+        </div>
+        <div>
+            <a href="loginform.php">Login</a>/<a href="registerform.php">Register</a>
+        </div>
     </body>
 </html>
---
+++ Php/languageform.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="../Icons/ionicons.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div>
+            <div><a href="index.php" class="backButton"><i class="fas fa-arrow-left"></i></a></div>
+            <div><h2>Choose your language</h2></div>
+        </div>
+        <div>
+            <p>I want to learn...</p>
+        </div>
+        <div>
+            <a href="mainmenu.php" class="enter">French</a>
+        </div>
+        <div>
+            <a href="mainmenu.php" class="enter">German</a>
+        </div>
+        </div>
+            <a href="mainmenu.php" class="enter">Irish</a>
+        </div>
+        <div>
+            <a href="mainmenu.php" class="enter">Italian</a>
+        </div>
+        </div>
+            <a href="mainmenu.php" class="enter">Blank</a>
+        </div>
+        <div>
+            <a href="mainmenu.php" class="enter">Blank</a>
+        </div>
+    </body>
+</html>
