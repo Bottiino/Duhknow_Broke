440ee834189b96432abbcb6b391261e36b6900b7
Bottino <adriancollins59@live.com>
2018-10-26 14:21:33 +0100

    changed para

--- Php/index.php
+++ Php/index.php
@@ -19,6 +19,6 @@ and open the template in the editor.
         <div>
             <a href="loginform.php">Login</a>/<a href="registerform.php">Register</a>
         </div>
-        <p>aoife smelly hashtag</p>
+        <p>blah</p>
     </body>
 </html>
