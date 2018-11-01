99b7d968f616db4328e5a714400468765aaed403
Sean Irwin <Sean Irwin@192.168.0.80>
2018-11-01 12:18:53 +0000

    Gets words function for dictionary + More eight words

--- Database/functions.php
+++ Database/functions.php
@@ -24,6 +24,70 @@ function getCategory()
 
         return $wordList;
     }
+    //Start of Get words functions for Dictionary
+    function getWords()
+    {        
+        global $db;
+        
+        $getWords = "SELECT * FROM words";
+        $statement = $db->prepare($getWords);      
+        $statement->execute();
+        $words = $statement->fetch();
+        $statement->closeCursor();
+
+        return $words;
+    }
+    function getWordsFrench()
+    {        
+        global $db;
+        
+        $getWords = "SELECT english, french FROM words";
+        $statement = $db->prepare($getWords);      
+        $statement->execute();
+        $words = $statement->fetch();
+        $statement->closeCursor();
+
+        return $words;
+    }
+    function getWordsGerman()
+    {        
+        global $db;
+        
+        $getWords = "SELECT english, german FROM words";
+        $statement = $db->prepare($getWords);      
+        $statement->execute();
+        $words = $statement->fetch();
+        $statement->closeCursor();
+
+        return $words;
+    }
+    function getWordsIrish()
+    {        
+        global $db;
+        
+        $getWords = "SELECT english, irish FROM words";
+        $statement = $db->prepare($getWords);      
+        $statement->execute();
+        $words = $statement->fetch();
+        $statement->closeCursor();
+
+        return $words;
+    }
+    function getWordsSpanish()
+    {        
+        global $db;
+        
+        $getWords = "SELECT english, spanish FROM words";
+        $statement = $db->prepare($getWords);      
+        $statement->execute();
+        $words = $statement->fetch();
+        $statement->closeCursor();
+
+        return $words;
+    }
+    //end
+    /////////////////////////////////////////////////////////////
+    //Functions for Game
     function getEightWords($categoryID)
     {        
         global $db;
@@ -37,3 +101,18 @@ function getCategory()
 
         return $eightWords;
     }
+    //If you dont want to extract what you want from previous we can do this for each
+    function getEightWordsFrench($categoryID)
+    {        
+        global $db;
+        
+        $getEight = "SELECT english, french FROM words JOIN word_wc on words.words_id = word_wc.words_id WHERE wc_id = :categoryID ORDER BY RANDOM() LIMIT 8";
+        $statement = $db->prepare($getEight);     
+        $statement->bindValue(':categoryID', $categoryID); 
+        $statement->execute();
+        $eightWords = $statement->fetch();
+        $statement->closeCursor();
+
+        return $eightWords;
+    }
+    
